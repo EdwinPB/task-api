11b4a5bd3fd286074b2d950107cb8c992f1a8238
EdwinPB <epolo@wawand.co>
2020-08-12 08:35:53 -0500

    added template box

--- actions/render.go
+++ actions/render.go
@@ -2,12 +2,15 @@ package actions
 
 import (
 	"github.com/gobuffalo/buffalo/render"
+	"github.com/gobuffalo/packr/v2"
 )
 
 var r *render.Engine
 
 func init() {
 	r = render.New(render.Options{
+		HTMLLayout:         "application.plush.html",
 		DefaultContentType: "application/json",
+		TemplatesBox:       packr.New("app:templates", "../../templates"),
 	})
 }
