02b6212a70120f8b938020cbf5af20433e3bd45c
EdwinPB <epolo@wawand.co>
2020-08-12 11:02:49 -0500

    removed line that was not needed when rendering

--- actions/render.go
+++ actions/render.go
@@ -9,7 +9,6 @@ var r *render.Engine
 
 func init() {
 	r = render.New(render.Options{
-		HTMLLayout:         "application.plush.html",
 		DefaultContentType: "application/json",
 		TemplatesBox:       packr.New("app:templates", "../templates"),
 	})
