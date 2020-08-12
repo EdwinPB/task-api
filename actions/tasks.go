733e5bfa49ad53bda5823ff809b82f1a7de1baae
EdwinPB <epolo@wawand.co>
2020-08-12 08:36:14 -0500

    removed printed line

--- actions/tasks.go
+++ actions/tasks.go
@@ -1,7 +1,6 @@
 package actions
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gobuffalo/buffalo"
@@ -9,6 +8,5 @@ import (
 
 // TasksList default implementation.
 func TasksList(c buffalo.Context) error {
-	fmt.Println("---_>")
 	return c.Render(http.StatusOK, r.HTML("tasks/list.html"))
 }
